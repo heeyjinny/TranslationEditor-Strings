b3d963d7f42dfaacb59220f3b03a5e98a718f374
heeyjinny <heeyjinny@gmail.com>
2022-11-08 22:21:23 +0900

    Initial commit

---
+++ app/src/main/java/com/heeyjinny/translationeditor_strings/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.heeyjinny.translationeditor_strings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
